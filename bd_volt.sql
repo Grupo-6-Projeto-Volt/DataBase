790017864990d4fe3893ec1cfedd4e32e206fe8c
AmandaCupolaa <amanda.alves@sptech.school>
2024-04-02 13:34:16 -0300

    Alteracoes nos nomes dos atributos

--- bd_volt.sql
+++ bd_volt.sql
@@ -14,78 +14,78 @@ use bd_volt;
 -- Usuario
 create table if not exists tb_usuario 
 (
-	id_usuario int primary key auto_increment,
-    nome_usuario varchar(100),
-    email_usuario varchar(100) unique,
-    telefone_usuario varchar(14),
-    cep_usuario char(9),
-    categoria_usuario binary
+	id int primary key auto_increment,
+    nome varchar(100),
+    email varchar(100) unique,
+    telefone varchar(14),
+    cep char(9),
+    categoria binary
 );
 select * from tb_usuario;
 
 -- Produto
 create table if not exists tb_produto
 (
-	id_produto int primary key auto_increment,
-    nome_produto varchar(80),
-    desc_produto varchar(300),
-    categ_produto varchar(100),
-    preco_produto decimal(8,2),
-    qnt_estoque int
+	id int primary key auto_increment,
+    nome varchar(80),
+    descricao varchar(300),
+    categoria varchar(100),
+    preco decimal(8,2),
+    qtd_estoque int
 );
 select * from tb_produto;
 
 -- Imagem produto
 create table if not exists tb_imagem_produto
 (
-	id_imagem_produto int primary key auto_increment,
-    nome_imagem varchar(100),
+	id int primary key auto_increment,
+    nome varchar(100),
     codigo_imagem blob,
     fk_produto int,
-    foreign key(fk_produto) references tb_produto(id_produto)
+    foreign key(fk_produto) references tb_produto(id)
 );
 select * from tb_imagem_produto	;
 
 -- Login
 create table if not exists tb_login
 (
-	id_login int primary key auto_increment,
-    email_login varchar(100) unique,
-    senha_login varchar(100) unique,
+	id int primary key auto_increment,
+    email varchar(100) unique,
+    senha varchar(100) unique,
     fk_usuario int,
-    foreign key(fk_usuario) references tb_usuario(id_usuario)
+    foreign key(fk_usuario) references tb_usuario(id)
 ); 
 select * from tb_login;
 
 -- Clique produto
 create table if not exists tb_click_produto
 (
-	id_click_produto int primary key auto_increment,
+	id int primary key auto_increment,
     data_hora_click datetime,
     possivel_compra binary,
 	fk_usuario int,
-    foreign key(fk_usuario) references tb_usuario(id_usuario),
+    foreign key(fk_usuario) references tb_usuario(id),
 	fk_produto int,
-    foreign key(fk_produto) references tb_produto(id_produto)
+    foreign key(fk_produto) references tb_produto(id)
 );
 select * from tb_click_produto;
 
 -- Tag produto
 create table if not exists tb_tag_produto
 (
-	id_tag_produto int primary key auto_increment,
-    tag_produto varchar(50)
+	id int primary key auto_increment,
+    tag varchar(50)
 );
 select * from tb_tag_produto;
 
 -- Classificação produto
 create table if not exists tb_classificacao_produto
 (
-	id_classificacao_produto int primary key auto_increment,
+	id int primary key auto_increment,
     fk_produto int,
-    foreign key(fk_produto) references tb_produto(id_produto),
+    foreign key(fk_produto) references tb_produto(id),
     fk_tag_produto int,
-    foreign key(fk_tag_produto) references tb_tag_produto(id_tag_produto)
+    foreign key(fk_tag_produto) references tb_tag_produto(id)
 );
 select * from tb_classificacao_produto;
 
