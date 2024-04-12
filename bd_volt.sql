7a8676e409cad9896e7dfa54f391d01884eace18
danielandrade108 <daniel.andrade@sptech.school>
2024-04-12 18:54:12 -0300

    feat: Adicionando novas tabelas, campo e inserts no Script

--- bd_volt.sql
+++ bd_volt.sql
@@ -1,4 +1,4 @@
--- drop database if exists bd_volt;
+drop database if exists bd_volt;
 create database if not exists bd_volt;
 use bd_volt;
 
@@ -8,10 +8,9 @@ use bd_volt;
 create table if not exists tb_usuario 
 (
 	id int primary key auto_increment,
-	nome varchar(100),
-	email varchar(100) unique,
-	telefone varchar(14),
-	cep char(9),
+	nome varchar(100) not null,
+	email varchar(100) not null unique,
+	telefone varchar(14) not null unique,
 	categoria tinyint(1)
 );
 
@@ -19,11 +18,12 @@ create table if not exists tb_usuario
 create table if not exists tb_produto
 (
 	id int primary key auto_increment,
-	nome varchar(80),
-	descricao varchar(300),
-	categoria varchar(100),
-	preco decimal(8,2),
-	qtd_estoque int
+	nome varchar(80) not null,
+	descricao varchar(300) not null,
+	categoria varchar(100) not null,
+	preco decimal(8,2) not null,
+	qtd_estoque int,
+    estado_geral varchar(45) not null
 );
 
 -- Imagem produto
@@ -40,8 +40,8 @@ create table if not exists tb_imagem_produto
 create table if not exists tb_login
 (
 	id VARCHAR(36) primary key,
-	email varchar(100) unique,
-	senha varchar(100) unique,
+	email varchar(255) unique,
+	senha varchar(16) not null unique,
 	fk_usuario int,
 	foreign key(fk_usuario) references tb_usuario(id)
 ); 
@@ -69,26 +69,44 @@ create table if not exists tb_tag_produto
 create table if not exists tb_classificacao_produto
 (
 	id int primary key auto_increment,
-	fk_produto int,
-	foreign key(fk_produto) references tb_produto(id),
 	fk_tag_produto int,
-	foreign key(fk_tag_produto) references tb_tag_produto(id)
+	foreign key(fk_tag_produto) references tb_tag_produto(id),
+	fk_produto int,
+	foreign key(fk_produto) references tb_produto(id)
 );
+
+create table if not exists tb_favoritos
+(
+	id int primary key auto_increment,
+    fk_usuario int,
+    foreign key(fk_usuario) references tb_usuario(id)
+);
+
+create table if not exists tb_lista_favoritos
+(
+	id int primary key auto_increment,
+    dt_hora_insercao datetime,
+    fk_favoritos int,
+    foreign key(fk_favoritos) references tb_favoritos(id),
+	fk_produto int,
+    foreign key(fk_produto) references tb_produto(id)
+);
+
 
 -- Inserts ------------------------------------------------------------
 
 -- Inserts para tabela tb_usuario
-INSERT INTO tb_usuario (nome, email, telefone, cep, categoria) VALUES
-('João Silva', 'joao@example.com', '123456789', '12345-678', 0),
-('Maria Santos', 'maria@example.com', '987654321', '54321-876', 0),
-('Pedro Oliveira', 'pedro@example.com', '111111111', '11111-111', 1),
-('Ana Souza', 'ana@example.com', '222222222', '22222-222', 0),
-('Lucas Fernandes', 'lucas@example.com', '333333333', '33333-333', 1),
-('Fernanda Lima', 'fernanda@example.com', '444444444', '44444-444', 0),
-('Gabriel Martins', 'gabriel@example.com', '555555555', '55555-555', 1),
-('Laura Oliveira', 'laura@example.com', '666666666', '66666-666', 0),
-('Rafaela Pereira', 'rafaela@example.com', '777777777', '77777-777', 1),
-('Marcos Almeida', 'marcos@example.com', '888888888', '88888-888', 0);
+INSERT INTO tb_usuario (nome, email, telefone, categoria) VALUES
+('João Silva', 'joao@example.com', '123456789', 0),
+('Maria Santos', 'maria@example.com', '987654321', 0),
+('Pedro Oliveira', 'pedro@example.com', '111111111', 1),
+('Ana Souza', 'ana@example.com', '222222222', 0),
+('Lucas Fernandes', 'lucas@example.com', '333333333', 1),
+('Fernanda Lima', 'fernanda@example.com', '444444444', 0),
+('Gabriel Martins', 'gabriel@example.com', '555555555', 1),
+('Laura Oliveira', 'laura@example.com', '666666666', 0),
+('Rafaela Pereira', 'rafaela@example.com', '777777777', 1),
+('Marcos Almeida', 'marcos@example.com', '888888888', 0);
 
 -- Inserts para tabela tb_login
 INSERT INTO tb_login (id, email, senha, fk_usuario) VALUES
@@ -104,17 +122,17 @@ INSERT INTO tb_login (id, email, senha, fk_usuario) VALUES
 (UUID(), 'marcos@example.com', 'senha282930', 10);
 
 -- Inserts para tabela tb_produto
-INSERT INTO tb_produto (nome, descricao, categoria, preco, qtd_estoque) VALUES
-('Notebook Dell', 'Notebook Dell Inspiron 15', 'Computador', 2500.00, 10),
-('Smartphone Samsung', 'Smartphone Samsung Galaxy S21', 'Celular', 1800.00, 15),
-('Teclado mecânico', 'Teclado mecânico para jogos', 'Acessório', 150.00, 20),
-('Mouse gamer', 'Mouse gamer com sensor óptico', 'Acessório', 80.00, 30),
-('Monitor LG', 'Monitor LG UltraWide 29', 'Acessório', 600.00, 5),
-('iPhone 13', 'Smartphone Apple iPhone 13', 'Celular', 2000.00, 12),
-('Headset Bluetooth', 'Headset Bluetooth JBL', 'Acessório', 120.00, 25),
-('Tablet Samsung', 'Tablet Samsung Galaxy Tab A7', 'Acessório', 400.00, 18),
-('Câmera Canon', 'Câmera digital Canon EOS Rebel T7', 'Acessório', 700.00, 8),
-('Notebook HP', 'Notebook HP Pavilion 14', 'Computador', 1800.00, 10);
+INSERT INTO tb_produto (nome, descricao, categoria, preco, qtd_estoque, estado_geral) VALUES
+('Notebook Dell', 'Notebook Dell Inspiron 15', 'Computador', 2500.00, 10, "Novo"),
+('Smartphone Samsung', 'Smartphone Samsung Galaxy S21', 'Celular', 1800.00, 15, "Semi novo"),
+('Teclado mecânico', 'Teclado mecânico para jogos', 'Acessório', 150.00, 20, "Novo"),
+('Mouse gamer', 'Mouse gamer com sensor óptico', 'Acessório', 80.00, 30, "Semi novo"),
+('Monitor LG', 'Monitor LG UltraWide 29', 'Acessório', 600.00, 5, "Novo"),
+('iPhone 13', 'Smartphone Apple iPhone 13', 'Celular', 2000.00, 12, "Novo"),
+('Headset Bluetooth', 'Headset Bluetooth JBL', 'Acessório', 120.00, 25, "Semi novo"),
+('Tablet Samsung', 'Tablet Samsung Galaxy Tab A7', 'Acessório', 400.00, 18, "Semi novo"),
+('Câmera Canon', 'Câmera digital Canon EOS Rebel T7', 'Acessório', 700.00, 8, "Novo"),
+('Notebook HP', 'Notebook HP Pavilion 14', 'Computador', 1800.00, 10, "Semi novo");
 
 -- Inserts para tabela tb_imagem_produto
 INSERT INTO tb_imagem_produto (nome, codigo_imagem, fk_produto) VALUES
@@ -173,6 +191,32 @@ INSERT INTO tb_classificacao_produto (fk_produto, fk_tag_produto) VALUES
 (9, 4), -- Câmera Canon é uma promoção
 (10, 5); -- Notebook HP é uma novidade
 
+INSERT INTO tb_favoritos (fk_usuario) VALUES 
+(1), 
+(3), 
+(5), 
+(7), 
+(9), 
+(2), 
+(4), 
+(6), 
+(8), 
+(10);
+
+INSERT INTO tb_lista_favoritos (dt_hora_insercao, fk_favoritos, fk_produto) VALUES
+('2024-04-12 08:00:00', 1, 2), -- Favorito 1, Produto 2
+('2024-04-12 08:10:00', 2, 4), -- Favorito 3, Produto 4
+('2024-04-12 08:20:00', 3, 6), -- Favorito 5, Produto 6
+('2024-04-12 08:30:00', 4, 8), -- Favorito 7, Produto 8
+('2024-04-12 08:40:00', 5, 10), -- Favorito 9, Produto 10
+('2024-04-12 08:50:00', 6, 1), -- Favorito 2, Produto 1
+('2024-04-12 09:00:00', 7, 3), -- Favorito 4, Produto 3
+('2024-04-12 09:10:00', 8, 5), -- Favorito 6, Produto 5
+('2024-04-12 09:20:00', 9, 7), -- Favorito 8, Produto 7
+('2024-04-12 09:30:00', 10, 9); -- Favorito 10, Produto 9
+
+
+
 -- Selects ------------------------------------------------------------
 select * from tb_usuario;
 select * from tb_login;
@@ -181,6 +225,9 @@ select * from tb_click_produto;
 select * from tb_imagem_produto;
 select * from tb_tag_produto;
 select * from tb_classificacao_produto;
+select * from tb_favoritos;
+select * from tb_lista_favoritos;
+
 
 -- Views --------------------------------------------------------------
 -- Procedures ---------------------------------------------------------
